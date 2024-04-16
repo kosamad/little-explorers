9d2942336cddcac7003cac03698141625b70fa8d
kosamad samad <KarenLSamad@gmail.com>
2024-04-16 13:25:53 +0000

    Base.html template with associated content/styling.

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialisation
+    var sidenav = document.querySelectorAll('.sidenav');
+     M.Sidenav.init(sidenav);
+  });
