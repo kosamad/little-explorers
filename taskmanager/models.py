b3ef76e6b9d70af62c580feb743feb0bd82b4de8
kosamad samad <KarenLSamad@gmail.com>
2024-04-16 10:59:36 +0000

    recommendation table and holiday type started with database construction. (not yet created).

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -4,41 +4,59 @@ from taskmanager import db
 
 # Database Tables
 
-# 1. Holiday Type table
-class Type(db.Model):
+# 1. Holiday Type table (cannot use work type as it is a reserved word in SQL)
+class Holiday(db.Model):
     #schema for the Type model
     id = db.Column(db.Integer, primary_key=True)
-    type_name = db.Column(db.String(15), unique=True, nullable=False) #!!!!! work out how manycharacters limit for responsive design
-
+    holiday_name = db.Column(db.String(15), unique=True, nullable=False) #!!!!! work out how manycharacters limit for responsive design
+    recommendation = db.relationship("Recommendation", backref="holiday", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # represent self in the form of a string
-        return self.type_name
+        return self.holiday_name
 
-# 2. Users table
-class Users(db.Model):
-    #schema for the Users model
-    id = db.Column(db.Integer, primary_key=True)
+# # 2. Users table
+# class Users(db.Model):
+#     #schema for the Users model
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(15), unique=True, nullable=False)
+#     email = db.Column(db.String(30), unique=True, nullable=False)
+#     password_hash 
 
 
-    def __repr__(self):
-        # __repr__ to represent itself in the form of a string
-        return self
+#     def __repr__(self):
+#         # __repr__ to represent itself in the form of a string
+#         return self
 
 # 3. Admin table
-class Admin(db.Model):
+#class Admin(db.Model):
     #schema for the Admin model
-    id = db.Column(db.Integer, primary_key=True)
+    #id = db.Column(db.Integer, primary_key=True)
+    #a_username
+   # a_email
 
-    def __repr__(self):
+    #def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return self
+      #  return self
 
 # 4. Recommendations table
-class Recommendations(db.Model):
-    #schema for the Recommendations model
+class Recommendation(db.Model):
+    # schema for the Recommendations model
     id = db.Column(db.Integer, primary_key=True)
-
+    recommendation_name = db.Column(db.String(25), unique=True, nullable=False)
+    # location_name = db.Column(db.String(25), nullable=False)
+    occupants = db.Column(db.Integer, nullable=False) 
+    recommendation_review = db.Column(db.Text, nullable=False)
+    holiday_id = db.Column(db.Integer, db.ForeignKey("holiday.id", ondelete="CASCADE"), nullable=False)
+    region = db.Column(db.String(25), nullable=False)
+    image = db.Column(db.Text, unique=True,) # hash it so this is the case. 
+    recommendation_date = db.Column(db.Date, nullable=False) 
+    #map_long = 
+    #map_lat
+    #user_id
+   
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return self
+        return "#{0} - Name:{1} | Date:{2}".format(
+            self.id, self.recommendation_name, self.recommendation_date
+        )
--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -2,6 +2,7 @@
 from flask import render_template
 # Import from taskmanager package
 from taskmanager import app, db
+from taskmanager.models import Holiday, Recommendation
 
 # Create Route Decorator
 @app.route("/")
