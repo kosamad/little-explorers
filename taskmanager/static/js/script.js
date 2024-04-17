8a4e1570fd5c2829b76ef8426a737b392bebedd2
kosamad samad <KarenLSamad@gmail.com>
2024-04-17 09:48:58 +0000

    Fixed Navbar style and logo

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialisation
     var sidenav = document.querySelectorAll('.sidenav');
      M.Sidenav.init(sidenav);
+
+    var dropdown = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(dropdown);
   });
