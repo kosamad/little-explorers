e2af8f5e22c637fba7b0b26790216b824cf685b4
kosamad samad <KarenLSamad@gmail.com>
2024-04-23 19:55:35 +0000

    Icon Styling fixed in form plus check to ensure both Icon and Name fields are completed correctly.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // sidenav initialisation
+      // sidenav initialisation
   var sidenav = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sidenav);
   // navbar dropdown initialisation
@@ -7,36 +7,35 @@ document.addEventListener('DOMContentLoaded', function () {
   M.Dropdown.init(dropdown);
 
   // icon dropdown
-
   var dropdownTrigger = document.getElementById('dropdown-trigger-two');
-  M.Dropdown.init(dropdownTrigger);
+  M.Dropdown.init(dropdownTrigger); 
 
-  // var iconDropdown = document.getElementById('icon-dropdown');
-  // var getIcon = document.getElementById('get-icon');
-
-  // iconDropdown.addEventListener('click', function (event) {
-  //   // Check if the clicked element is an <i> tag
-  //   if (event.target.classList.contains('icon-option')) {
-  //     // Get the icon class from the data-icon attribute
-  //     var selectedIcon = event.target.getAttribute('data-icon');
-  //     // Update the selected icon div
-  //     getIcon.innerHTML = '<input id="selected_icon" name="selected_icon" type="text" required><label for="selected_icon">Selected Icon:    <i class="' + selectedIcon  + '"></i></label>';
-  //     console.log(iconClass);
-  //   }
-  // });
-
+  // Displaying the icon to the user and storing it in the database
   var iconDropdown = document.getElementById('icon-dropdown');
   var selectedIconHidden = document.getElementById('selected_icon');
   var selectedIconDisplay = document.getElementById('selected_icon_display');
-
+  var iconClass = '';
+  
   iconDropdown.addEventListener('click', function (event) {
     if (event.target.classList.contains('icon-option')) {
-      var iconClass = event.target.getAttribute('data-icon');
+      
+      iconClass = event.target.getAttribute('data-icon');
       // Update the hidden input field with the selected icon class
       selectedIconHidden.value = iconClass;
       // Display the selected icon to the user
-      selectedIconDisplay.innerHTML = '<label for="display_only">Icon</label><input id="display_only" name="display_only" type="text" readonly><i class="' + iconClass + '"></i>';
+      selectedIconDisplay.innerHTML = '<input id="display_only" name="display_only" type="text" readonly class="validate" required><label for="display_only">Icon Selected: <i class="' + iconClass + '"></i></label>';
+      // Prevent user clicking in the icon box
+      var displayInput = document.getElementById('display_only');
+      displayInput.disabled = true;
     }
   });
 
-});
+})
+
+function checkMaxLength(input) {
+  if (input.value.length > input.maxLength) {
+      input.value = input.value.slice(0, input.maxLength);
+  }
+}
+
+  
