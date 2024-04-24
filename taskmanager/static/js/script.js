409a78d3a1e3f8168cea01f4a8811226bdf606ad
kosamad samad <KarenLSamad@gmail.com>
2024-04-24 13:51:34 +0000

    Select initialisation for adding holiday type to recommendation page.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,11 +1,14 @@
-
 document.addEventListener('DOMContentLoaded', function () {
   // sidenav initialisation
-  var sidenav = document.querySelectorAll('.sidenav');
+  let sidenav = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sidenav);
   // navbar dropdown initialisation
-  var dropdown = document.querySelectorAll('.dropdown-trigger');
+  let dropdown = document.querySelectorAll('.dropdown-trigger');
   M.Dropdown.init(dropdown);
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
 
   // icon dropdown
   var dropdownTrigger = document.getElementById('dropdown-trigger-two');
@@ -17,7 +20,6 @@ document.addEventListener('DOMContentLoaded', function () {
   var selectedIconDisplay = document.getElementById('selected_icon_display');
   var iconClass = '';
 
-
   iconDropdown.addEventListener('click', function (event) {
     if (event.target.classList.contains('icon-option')) {
       iconClass = event.target.getAttribute('data-icon');
@@ -27,9 +29,14 @@ document.addEventListener('DOMContentLoaded', function () {
       selectedIconDisplay.innerHTML = '<input id="display_only" name="display_only" type="text" readonly class="validate" required><label for="display_only">Icon Selected: <i class="' + iconClass + '"></i></label>';
       // Prevent user clicking in the icon box
       var displayInput = document.getElementById('display_only');
-      displayInput.disabled = true;     
-         }
+      displayInput.disabled = true;
+    }
   });
+
+
+
+
+
 
 })
 
