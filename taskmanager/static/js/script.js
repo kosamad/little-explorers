c561619590173de542a0947e7e325186c22f2f7d
kosamad samad <KarenLSamad@gmail.com>
2024-04-26 19:41:46 +0000

    Maps JS separated into different file. Basic map centralized on the UK in div.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -81,33 +81,3 @@ function checkMaxLength30(input) {
   }
 }
 
-// Google Maps Elements
-
-function initialize() {
-
-	var mapOptions, map, marker,		
-		addressEl = document.querySelector( '#location_name' ),
-		latEl = document.querySelector( '#map_lat' ),
-		longEl = document.querySelector( '#map_long' ),		
-    element = document.getElementById( 'map-canvas' );	
-
-	mapOptions = {
-		// How far the maps zooms in.
-		zoom: 8,
-		// Current Lat and Long position of the pin
-		center: new google.maps.LatLng( 18.5204, 73.8567 ),
-		// center : {
-		// 	lat: -34.397,
-		// 	lng: 150.644
-		// },
-		disableDefaultUI: false, // Disables the controls like zoom control on the map if set to true
-		scrollWheel: true, // If set to false disables the scrolling on the map.
-		draggable: true, // If set to false , you cannot move the map around.
-		// mapTypeId: google.maps.MapTypeId.HYBRID, // If set to HYBRID its between sat and ROADMAP, Can be set to SATELLITE as well.
-		// maxZoom: 11, // Wont allow you to zoom more than this
-		// minZoom: 9  // Wont allow you to go more up.
-	};
-}
-
-//creating an object map with the constructor function map()
-map = new google.mapsMap( element, mapOptions);
