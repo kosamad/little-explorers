4fac62aa9f0a3f5bdd8ffa46091905a27e33956c
kosamad samad <KarenLSamad@gmail.com>
2024-04-29 08:53:29 +0000

    Marker movements update address and lat/lng boxs. Boxes hidden from user.

--- taskmanager/static/js/maps.js
+++ taskmanager/static/js/maps.js
@@ -1,3 +1,5 @@
+// Code for map, search box and geocoding modified from the youtube tutorial by Imran Sayed using the methods by Google's API
+
 // Google Maps Elements, initialise function and define variables. 
 function initialize() {
   var mapOptions, map, marker, searchBox,
@@ -39,29 +41,65 @@ function initialize() {
   searchBox = new google.maps.places.SearchBox(addressEl);
 
   // when address is changed and selected, place the marker at that location. 
-  google.maps.event.addListener( searchBox, 'places_changed', function () {
-		
+  google.maps.event.addListener(searchBox, 'places_changed', function () {
+
     var places = searchBox.getPlaces(), //gets the place info (address etc) from the searchbox
-			bounds = new google.maps.LatLngBounds(), //gives lat and long values.
-			i, place, lat, long, resultArray,
-			addresss = places[0].formatted_address;
+      bounds = new google.maps.LatLngBounds(), //gives lat and long values.
+      i, place, lat, long,
+      address = places[0].formatted_address;
 
-		for( i = 0; place = places[i]; i++ ) {
-			bounds.extend( place.geometry.location );
-			marker.setPosition( place.geometry.location );  // Sets marker position
-		}
+    for (i = 0; place = places[i]; i++) {
+      bounds.extend(place.geometry.location);
+      marker.setPosition(place.geometry.location); // Sets marker position
+    }  
 
-    map.fitBounds( bounds );  // adjust viewport to where the bounds are. 
-		map.setZoom( 15 ); 
+    map.fitBounds(bounds); // adjust viewport to where the bounds are. 
+    map.setZoom(15);
     //getting lat and long data from the map
     lat = marker.getPosition().lat();
-		long = marker.getPosition().lng();
-		latEl.value = lat;
-		longEl.value = long;
-		
+    long = marker.getPosition().lng();
+    latEl.value = lat;
+    longEl.value = long;
+  })
 
-})
+  // Capturing the marker information if the user moves the marker on the map. 
+  google.maps.event.addListener(marker, "dragend", function (event) {
+    var lat, long, address;
+    lat = marker.getPosition().lat();
+    long = marker.getPosition().lng();
 
-// into a hidden box, collect address using places[3].short_name
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+      latLng: marker.getPosition()
+    }, function (result, status) {
+      if ('OK' === status) {
+        address = result[0].formatted_address;
+        //updated address, lat and long parameters in HTML
+        addressEl.value = address;
+        latEl.value = lat;
+        longEl.value = long;
 
-}
+      } else {
+        console.log('Geocode was not successful for the following reason: ' + status);
+      }
+
+
+
+
+      // Closes the previous info window if it already exists
+      // if (infoWindow) {
+      //   infoWindow.close();
+      // }
+
+      /**
+       * Creates the info Window at the top of the marker need to put in first function if I want it. 
+      //  */
+      // infoWindow = new google.maps.InfoWindow({
+      //   content: address
+      // });
+
+      // infoWindow.open(map, marker);
+    });
+  })
+
+};
