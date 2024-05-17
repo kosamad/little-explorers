a06b535994b962d9cd90ae4336e9133fb6848a92
kosamad samad <KarenLSamad@gmail.com>
2024-05-17 14:59:03 +0000

    Function so users cannot submit until their passwords match.

--- taskmanager/static/js/createAccount.js
+++ taskmanager/static/js/createAccount.js
@@ -4,11 +4,14 @@
         var password = document.getElementById("password_hash").value;
         var confirmPassword = document.getElementById("password_hash2").value;
         var errorDiv = document.getElementById("password_error");
+        var submitButton = document.getElementById("submit_button");
 
         if (password !== confirmPassword) {
             errorDiv.textContent = "Passwords do not match";
+            submitButton.disabled = true; 
         } else {
             errorDiv.textContent = "";
+            submitButton.disabled = false;
         }
     }
 
